d6b64b8069348e66e9356395fd1f128a49095844
Jamie Birch work laptop <jbizzle5@gmail.com>
2017-11-11 12:32:20 +0000

    fields marked as nullable (so Swift correctly interprets them as optionals).

--- LibMecabSample/Node.h
+++ LibMecabSample/Node.h
@@ -10,33 +10,54 @@
 #import "../mecab/mecab.h" // or #import <mecab.h> to use globally installed one
 
 @interface Node : NSObject {
-	NSString *surface;
-	NSString *feature;
-	
-	NSArray<NSString *> *features;
+    NSString *feature;
+    NSArray<NSString *> *features;
+    
+    NSString *surface; // originally provided
+    NSString *partOfSpeech;
+    NSString *partOfSpeechSubtype1;
+    NSString *partOfSpeechSubtype2;
+    NSString *partOfSpeechSubtype3;
+    NSString *inflection;
+    NSString *useOfType;
+    NSString *originalForm;
+    NSString *reading;
+    NSString *pronunciation;
 }
 
-@property (nonatomic, retain) NSString *surface;
-@property (nonatomic, retain) NSString *feature;
-@property (nonatomic, retain) NSArray<NSString *> *features;
+
+@property (nonatomic, retain, nullable) NSString *feature;
+@property (nonatomic, retain, nullable) NSArray<NSString *> *features;
+
+@property (nonatomic, retain, nonnull) NSString *surface; // originally provided
+@property (nonatomic, retain, nullable) NSString *partOfSpeech;
+@property (nonatomic, retain, nullable) NSString *partOfSpeechSubtype1;
+@property (nonatomic, retain, nullable) NSString *partOfSpeechSubtype2;
+@property (nonatomic, retain, nullable) NSString *partOfSpeechSubtype3;
+@property (nonatomic, retain, nullable) NSString *inflection;
+@property (nonatomic, retain, nullable) NSString *useOfType;
+@property (nonatomic, retain, nullable) NSString *originalForm;
+@property (nonatomic, retain, nullable) NSString *reading;
+@property (nonatomic, retain, nullable) NSString *pronunciation;
 
 // 品詞
-- (NSString *)partOfSpeech;
+- (nullable NSString *)partOfSpeech;
 // 品詞細分類1
-- (NSString *)partOfSpeechSubtype1;
+- (nullable NSString *)partOfSpeechSubtype1;
 // 品詞細分類2
-- (NSString *)partOfSpeechSubtype2;
+- (nullable NSString *)partOfSpeechSubtype2;
 // 品詞細分類3
-- (NSString *)partOfSpeechSubtype3;
+- (nullable NSString *)partOfSpeechSubtype3;
 // 活用形
-- (NSString *)inflection;
+- (nullable NSString *)inflection;
 // 活用型
-- (NSString *)useOfType;
+- (nullable NSString *)useOfType;
 // 原形
-- (NSString *)originalForm;
+- (nullable NSString *)originalForm;
 // 読み
-- (NSString *)reading;
+- (nullable NSString *)reading;
 // 発音
-- (NSString *)pronunciation;
-	
+- (nullable NSString *)pronunciation;
+
 @end
+
