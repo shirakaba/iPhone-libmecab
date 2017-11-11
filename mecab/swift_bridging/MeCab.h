f964ced746dbacc3138c7d8f57818eb726d8d1bd
Jamie Birch work laptop <jbizzle5@gmail.com>
2017-11-11 17:21:54 +0000

    cleanup of project structure

--- mecab/swift_bridging/MeCab.h
+++ mecab/swift_bridging/MeCab.h
@@ -6,7 +6,8 @@
 //
 //
 
-#import <UIKit/UIKit.h>
+// #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MeCab.
 FOUNDATION_EXPORT double MeCabVersionNumber;
@@ -16,4 +17,15 @@ FOUNDATION_EXPORT const unsigned char MeCabVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MeCab/PublicHeader.h>
 
+// #import "../objc_bridging/Mecab.h"
+// #import "../objc_bridging/Node.h"
+
+// #import <MeCab/Mecab.h>
+// #import <MeCab/Node.h>
+
+// #import <MeCab/objc_bridging/Mecab.h>
+// #import <MeCab/objc_bridging/Node.h>
+
+#import <mecab/Mecab.h>
+#import <mecab/Node.h>
 
