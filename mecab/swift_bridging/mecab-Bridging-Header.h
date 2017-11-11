cdaff5092a48092e8a802a13026a5087a7723ce5
Jamie Birch work laptop <jbizzle5@gmail.com>
2017-11-11 22:02:16 +0000

    notes on bridging header

--- mecab/swift_bridging/mecab-Bridging-Header.h
+++ mecab/swift_bridging/mecab-Bridging-Header.h
@@ -1,5 +1,8 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
+//  This is not automatically picked up by a consumer of the static library if simply exposed as a
+//  public header; the consumer needs to set it explicitly as the bridging header for its target.
+//  May be able to get around that by making a framework.
 //
 
 
