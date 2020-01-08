2ef219ba1e5cfdaa4faf0521bed6eb40f0c27e40
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-01-08 23:03:36 +0000

    Add pod-tester project for testing the pods I'll be making

---
+++ pod-tester/pod-tester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pod-tester
+//
+//  Created by jamie on 08/01/2020.
+//  Copyright © 2020 Bottled Logic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
