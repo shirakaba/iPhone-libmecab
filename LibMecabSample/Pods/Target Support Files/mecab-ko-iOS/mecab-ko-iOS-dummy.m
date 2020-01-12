1d83869ff9132c2c313064e8e720a6a00ffbadc0
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-01-12 15:45:13 +0000

    update pods

--- LibMecabSample/Pods/Target Support Files/mecab-ko-iOS/mecab-ko-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_mecab_ko_iOS : NSObject
-@end
-@implementation PodsDummy_mecab_ko_iOS
-@end
--- LibMecabSample/Pods/Target Support Files/mecab-ko-macOS/mecab-ko-macOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_mecab_ko_macOS : NSObject
-@end
-@implementation PodsDummy_mecab_ko_macOS
-@end
